5905984bb2374776f13b04d8e4813a3a3f7674e1
busankimchi <jacky185@kaist.ac.kr>
2020-07-24 23:42:53 +0900

    modify API

--- src/database/api.ts
+++ src/database/api.ts
@@ -3,7 +3,12 @@ import firestore from '@react-native-firebase/firestore';
 const db = firestore();
 
 export function getListAll(date: string) {
-    return db.collection('todos').doc(date).collection('items').get();
+    return db
+        .collection('todos')
+        .doc(date)
+        .collection('items')
+        .orderBy('time')
+        .get();
 }
 
 export function getItem(date: string, id: string) {
